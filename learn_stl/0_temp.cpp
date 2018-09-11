8d3f4eb842c430d2d2912ee8b1de8a8c4456aabc
Arjen_mac <friendship0130@yeah.net>
2018-09-11 20:58:01 +0800

    模板文件，作为创建新的源文件时的模板。

---
+++ learn_stl/0_temp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+class LearnString
+{
+public:
+    void string()
+    {
+
+    }
+};
+
+
+int main()
+{
+    
+
+    return 0;
+}
