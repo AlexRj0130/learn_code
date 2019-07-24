a308c2e611dc8b661146c031608b9531a9bb075c
zhangrunjie <zhangrunjie@bytedance.com>
2019-07-24 23:08:29 +0800

    init algorithm folder

---
+++ algorithm/bin_tree.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+class BinTreeNode 
+{
+public:
+    BinTreeNode(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+
+    BinTreeNode(int data, BinTreeNode * left, BinTreeNode * right)
+    {
+        this->data = data;
+        this->left = left;
+        this->right = right;
+    }
+
+private:
+    int data;
+    BinTreeNode * left;
+    BinTreeNode * right;
+
+};
+
+class BinTreeTranverse
+{
+    void FirstOrder(BinTreeNode * root)
+    {
+        if (root == NULL)
+            return;
+
+        
+    }
+
+    void MidOrder(BinTreeNode * root)
+    {
+
+    }
+
+    void LastOrder(BinTreeNode * root)
+    {
+
+    }
+}
+
+int main()
+{
+    return 0;
+}
