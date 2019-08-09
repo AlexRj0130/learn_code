7cd96ff91129a4434dfd0614a6e960bf4a749db3
Runjie Zhang (FA Talent) <v-runzh@microsoft.com>
2019-08-09 12:28:38 +0800

    add folder of CSharp

---
+++ learn_csharp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace learn_csharp
+{
+    class Program
+    {
+
+        static void ThrowException()
+        {
+            throw new ArgumentNullException();
+        }
+
+        static void CatchExceptionNotThrowAgain()
+        {
+            try
+            {
+                ThrowException();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0} Exception caught", e);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // try
+            // {
+            //     ThrowException();
+            // }
+            // catch (Exception e)
+            // {
+            //     Console.WriteLine("{0} Exception caught", e);
+            // }
+            try
+            {
+                CatchExceptionNotThrowAgain();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0} Inner Exception caught", e);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
